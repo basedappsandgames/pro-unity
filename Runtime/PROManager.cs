f32322a16aa54be66a4b8d1d4f7248af5966541b
khoi <khoivietle2@gmail.com>
2025-09-08 23:59:49 -0700

    preserve fix

--- Runtime/PROManager.cs
+++ Runtime/PROManager.cs
@@ -425,7 +425,7 @@ namespace Wildwest.Pro
         public class PROModerationResponse
         {
             // constructor
-            [JsonConstructor, Preserve]
+            [JsonConstructor, UnityEngine.Scripting.Preserve]
             public PROModerationResponse(PROModerationResponseItem[] results, ErrorResponse error)
             {
                 this.results = results;
@@ -445,7 +445,7 @@ namespace Wildwest.Pro
         public class PROModerationResponseItem
         {
             // constructor
-            [JsonConstructor, Preserve]
+            [JsonConstructor, UnityEngine.Scripting.Preserve]
             public PROModerationResponseItem(
                 PROModerationAnalysis a,
                 string fileUrl,
@@ -484,7 +484,7 @@ namespace Wildwest.Pro
         public class PROModerationAnalysis
         {
             // constructor
-            [JsonConstructor, Preserve]
+            [JsonConstructor, UnityEngine.Scripting.Preserve]
             public PROModerationAnalysis(string ta, Dictionary<string, double> e)
             {
                 this.ta = ta;
